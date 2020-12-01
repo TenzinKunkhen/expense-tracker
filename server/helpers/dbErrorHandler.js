7f9f3e1ecb0a8c49a70a4ad8a6d2d6303b9116ce
TenzinKunkhen <kunkhen.tenzin@gmail.com>
2020-12-01 14:37:16 -0500

    added error handling in DB

---
+++ server/helpers/dbErrorHandler.js
@@ -0,0 +1,36 @@
+//set error messages when saving to the DB
+
+const getErrorMessage = (err) => {
+  let message = "";
+  if (err.code) {
+    switch (err.code) {
+      case 11000:
+      case 11001:
+        message = getUniqueErrorMessage(err);
+        break;
+      default:
+        message = "something went wrong";
+    }
+  } else {
+    for (let errName in err.errors) {
+      if (err.errors[errName].message) message = err.errors[errName].message;
+    }
+  }
+  return message;
+};
+export default { getErrorMessage };
+
+//set unique errors i.e. unique field
+const getUniqueErrorMessage = (err) => {
+  let output;
+  try {
+    let fieldName = err.message.substring(
+      err.message.lastIndexOf(".$") + 2,
+      err.message.lastIndexOf("_1")
+    );
+    output =  fieldName.charAt(0).toUpperCase() + fieldName.slice(1) + "already exists";
+  } catch (ex) {
+    output = "Unique field already exists";
+  }
+  return output;
+};
