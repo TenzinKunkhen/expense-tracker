efd170f4f73bfc97f439f6cb288e12fd8ca4f8de
TenzinKunkhen <kunkhen.tenzin@gmail.com>
2020-12-01 17:48:09 -0500

    finished user CRUD

--- server/express.js
+++ server/express.js
@@ -5,17 +5,17 @@ import compress from 'compression'
 import cors from 'cors'
 import helmet from 'helmet'
 import Template from '../template';
+import userRoutes from './routes/user.routes';
 
 const app = express()
 
-app.get('/', (req,res) => {
-    res.status(200).send(Template())
-})
+
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(compress())
 app.use(helmet())
 app.use(cors())
-
+app.use('/', userRoutes);
 export default app
---
+++ server/routes/user.routes.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import userCtrl from '../controllers/user.controller';
+
+const router = express.Router();
+
+// /api/users for the following:
+
+// Listing users with GET
+// Creating a new user with POST
+router.route('/api/users')
+.get(userCtrl.list)
+.post(userCtrl.create)
+
+// /api/users/:userId for the following:
+// Fetching a user with GET
+// Updating a user with PUT
+// Deleting a user with DELETE
+
+router.route('/api/users.:userId')
+.get(userCtrl.read)
+.put(userCtrl.update)
+.delete(userCtrl.remove)
+
+router.param('userId', userCtrl.userByID);
+
+export default router;
